562f5aed46db147335abbcdaf477433a7409b52a
Devensh22345 <kumardevensh0@gmail.com>
2025-04-28 11:20:26 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -3,25 +3,32 @@ from info import *
 from utils import *
 from time import time
 from client import User
-from pyrogram import Client, filters 
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.text & filters.group & filters.incoming & ~filters.command(["verify", "connect", "id"]))
 async def search(bot, message):
-    f_sub = await force_sub(bot, message)
-    if f_sub==False:
-       return     
-    channels = (await get_group(message.chat.id))["channels"]
-    if bool(channels)==False:
-       return     
-    if message.text.startswith("/"):
-       return    
-    query   = message.text
     try:
-       for channel in channels:
-           async for msg in User.search_messages(chat_id=channel, query=query):
-               # Forward the message instead of sending a link
-               await msg.forward(message.chat.id)
-               break  # Stop after forwarding the first matching message
-    except :
-       pass
+        # Check force subscribe
+        if await force_sub(bot, message) == False:
+            return
+
+        # Get associated channels for this group
+        group_data = await get_group(message.chat.id)
+        channels = group_data.get("channels", [])
+        if not channels:
+            return
+
+        # Ignore commands
+        if message.text.startswith("/"):
+            return
+
+        query = message.text
+
+        # Search and forward the first matching message
+        for channel in channels:
+            async for msg in User.search_messages(chat_id=channel, query=query):
+                await msg.forward(message.chat.id)
+                break  # Forward only the first match per query
+    except Exception as e:
+        print(f"Error in search function: {e}")
